5731a59a6abe405a56da5afd7a36409ea05b719a
Guilherme Quaglio <guilherme.silva@araguaia.com.br>
2026-09-09 14:22:19 -0300

    chore: adiciona dependências e configuração do banco SQLite em memória

---
+++ app/database.py
@@ -0,0 +1,31 @@
+from collections.abc import Generator
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+from sqlalchemy.pool import StaticPool
+
+engine = create_engine(
+    "sqlite://",
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    """Classe base declarativa para todos os modelos ORM."""
+
+
+def get_session() -> Generator[Session, None, None]:
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+
+
+def create_tables() -> None:
+    from app import models
+
+    Base.metadata.create_all(bind=engine)
